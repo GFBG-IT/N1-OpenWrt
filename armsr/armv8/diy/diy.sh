f695334fd90c30c129df4ad4327f927e2493fee9
GFBG-IT <44032665+GFBG-IT@users.noreply.github.com>
2026-02-24 20:37:51 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -5,12 +5,13 @@ patch -p1 -f < $(dirname "$0")/luci.patch
 
 # Clone packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 clone/passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 --depth=1 clone/passwall2
 git clone https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman --depth=1 feeds/luci/applications/luci-app-podman
+git
 
 # Adjust packages
 rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall2 feeds/luci/applications/
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
