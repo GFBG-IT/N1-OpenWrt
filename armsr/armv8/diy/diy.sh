15b339ba3a94830df7a82ef7692367d952c163a0
GFBG-IT <44032665+GFBG-IT@users.noreply.github.com>
2026-02-26 00:17:19 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -5,8 +5,11 @@ patch -p1 -f < $(dirname "$0")/luci.patch
 
 # Clone packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+git clone https://github.com/sbwml/autocore-arm --depth=1 --branch openwrt-24.10 clone/autocore-arm
+
 # Adjust packages
-cp -rf clone/amlogic/luci-app-amlogic  feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
+cp -rf clone/autocore-arm feeds/luci/applications/autocore-arm
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
