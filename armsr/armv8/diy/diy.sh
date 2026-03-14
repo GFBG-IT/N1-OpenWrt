e8eccd98d355c3cb0b2d8993cff36b1435dca955
GFBG-IT <44032665+GFBG-IT@users.noreply.github.com>
2026-03-14 18:27:56 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -8,7 +8,6 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/sbwml/autocore-arm --depth=1 --branch openwrt-25.12 clone/autocore-arm
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 --depth=1 clone/passwall2
 git clone https://github.com/sbwml/luci-app-mosdns --depth=1 --branch v5 clone/mosdns
 git clone https://github.com/linkease/ddnstox-openwrt-package --depth=1 clone/ddnstox
 git clone https://github.com/nikkinikki-org/OpenWrt-nikki --depth=1 clone/nikki
@@ -16,11 +15,8 @@ git clone https://github.com/timsaya/luci-app-bandix --depth=1 clone/luci-bandix
 git clone https://github.com/timsaya/openwrt-bandix --depth=1 clone/core-bandix 
 
 # Adjust packages 
-
 cp -rf clone/autocore-arm/* feeds/packages/utils/autocore/
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
-rm -rf feeds/luci/applications/luci-app-passwall2
-cp -rf clone/passwall2/luci-app-passwall2 feeds/luci/applications/
 rm -rf feeds/luci/applications/luci-app-mosdns
 cp -rf clone/mosdns/v2dat feeds/packages/utils/v2dat
 cp -rf clone/mosdns/luci-app-mosdns feeds/luci/applications/
