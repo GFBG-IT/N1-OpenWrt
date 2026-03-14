13a1049a7ee667d11addf534f10c9aa26c7114a7
GFBG-IT <44032665+GFBG-IT@users.noreply.github.com>
2026-03-14 20:21:57 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -6,26 +6,12 @@ patch -p1 -f < $(dirname "$0")/luci.patch
 # Clone packages
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
-git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/sbwml/autocore-arm --depth=1 --branch openwrt-25.12 clone/autocore-arm
-git clone https://github.com/sbwml/luci-app-mosdns --depth=1 --branch v5 clone/mosdns
-git clone https://github.com/linkease/ddnstox-openwrt-package --depth=1 clone/ddnstox
-git clone https://github.com/nikkinikki-org/OpenWrt-nikki --depth=1 clone/nikki
-git clone https://github.com/timsaya/luci-app-bandix --depth=1 clone/luci-bandix 
-git clone https://github.com/timsaya/openwrt-bandix --depth=1 clone/core-bandix 
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 
 # Adjust packages 
 cp -rf clone/autocore-arm/* feeds/packages/utils/autocore/
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
-rm -rf feeds/luci/applications/luci-app-mosdns
-cp -rf clone/mosdns/v2dat feeds/packages/utils/v2dat
-cp -rf clone/mosdns/luci-app-mosdns feeds/luci/applications/
-cp -rf clone/ddnstox/luci-app-ddnstox feeds/luci/applications/
-cp -rf clone/nikki/luci-app-nikki feeds/luci/applications/
-cp -rf clone/luci-bandix/luci-app-bandix feeds/luci/applications/
-cp -rf clone/ddnstox feeds/packages/net/ddnstox
-cp -rf clone/nikki feeds/packages/net/nikki
-cp -rf clone/core-bandix feeds/packages/net/openwrt-bandix 
 
 # Remove conflicting package
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
