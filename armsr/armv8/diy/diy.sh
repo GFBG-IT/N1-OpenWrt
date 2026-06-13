4d34a31fbdbe044f515b16d5e826d886e8c57df7
GFBG-IT <44032665+GFBG-IT@users.noreply.github.com>
2026-06-14 00:05:45 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -3,15 +3,22 @@
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
 
-# Clone packages
+# Remove
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/autocore-arm --depth=1 --branch openwrt-25.12 clone/autocore-arm
-git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+rm -rf feeds/luci/applications/luci-app-daed
+
+# Clone packages
+git clone --depth=1 -b 26.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+git clone --depth=1 --branch openwrt-25.12 https://github.com/sbwml/autocore-arm clone/autocore-arm
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic clone/amlogic
+git clone --depth=1 https://github.com/QiuSimons/luci-app-daed clone/daed
+git clone --depth=1 https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman clone/podman
 
 # Adjust packages 
-cp -rf clone/autocore-arm/* feeds/packages/utils/autocore/
+cp -rf clone/autocore-arm feeds/packages/utils/autocore/
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
+cp -rf clone/daed/luci-app-daed feeds/luci/applications/
+cp -rf clone/podman feeds/luci/applications/luci-app-podman
 
 # Remove conflicting package
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
