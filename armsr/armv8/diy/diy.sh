9d95afbdf81692c8b818a6a1a4fbd6df733da4a7
GFBG-IT <44032665+GFBG-IT@users.noreply.github.com>
2026-06-26 20:58:11 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -12,13 +12,11 @@ git clone --depth=1 -b 26.x https://github.com/sbwml/packages_lang_golang feeds/
 git clone --depth=1 --branch openwrt-25.12 https://github.com/sbwml/autocore-arm clone/autocore-arm
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic clone/amlogic
 git clone --depth=1 https://github.com/QiuSimons/luci-app-daed clone/daed
-git clone --depth=1 https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman clone/podman
 
 # Adjust packages 
 cp -rf clone/autocore-arm feeds/packages/utils/autocore/
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
 cp -rf clone/daed/luci-app-daed feeds/luci/applications/
-cp -rf clone/podman feeds/luci/applications/luci-app-podman
 
 # Remove conflicting package
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
