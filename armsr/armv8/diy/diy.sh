6531a69748b397367a8abc09efaaa33a86444573
darks <yeemyfh@gmail.com>
2026-07-08 21:11:06 +0800

    update

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -13,7 +13,6 @@ rm -rf feeds/luci/applications/luci-app-daed
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/net/v2ray-plugin
 rm -rf feeds/packages/net/xray-plugin
 rm -rf feeds/packages/net/chinadns-ng
 rm -rf feeds/packages/net/dns2socks
@@ -23,8 +22,6 @@ rm -rf feeds/packages/net/ipt2socks
 rm -rf feeds/packages/net/microsocks
 rm -rf feeds/packages/net/naiveproxy
 rm -rf feeds/packages/net/shadowsocks-rust
-rm -rf feeds/packages/net/shadowsocksr-libev
-rm -rf feeds/packages/net/simple-obfs
 rm -rf feeds/packages/net/tcping
 rm -rf feeds/packages/net/geoview
 rm -rf feeds/packages/net/shadow-tls
@@ -52,6 +49,12 @@ git clone --depth=1 https://github.com/linkease/ddnstox-openwrt-package clone/dd
 git clone --depth=1 https://github.com/timsaya/openwrt-bandix clone/openwrt-bandix
 git clone --depth=1 https://github.com/timsaya/luci-app-bandix clone/luci-app-bandix
 
+# openlist2 (alist variant file list)
+git clone --depth=1 https://github.com/sbwml/luci-app-openlist2 clone/openlist2
+
+# nikki (transparent proxy with mihomo)
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki clone/nikki
+
 # ---- Integrate packages into feeds ----
 
 # Original packages
@@ -67,17 +70,20 @@ cp -rf clone/mosdns/v2dat feeds/packages/net/
 # v2ray-geodata (sbwml version, replaces upstream)
 cp -rf clone/v2ray-geodata feeds/packages/net/v2ray-geodata
 
-# passwall (exclude v2ray-geodata, use sbwml version instead)
+# passwall (exclude v2ray-geodata, use sbwml version instead; also skip unused backends)
+SKIP_PKGS="v2ray-geodata shadowsocksr-libev simple-obfs v2ray-plugin haproxy"
 for pkg in clone/passwall-packages/*/; do
     pkg_name=$(basename "$pkg")
-    if [ "$pkg_name" != "v2ray-geodata" ]; then
-        cp -rf "$pkg" feeds/packages/net/
-    fi
+    skip=0
+    for sp in $SKIP_PKGS; do
+        [ "$pkg_name" = "$sp" ] && skip=1 && break
+    done
+    [ "$skip" -eq 0 ] && cp -rf "$pkg" feeds/packages/net/
 done
 cp -rf clone/passwall-luci/luci-app-passwall feeds/luci/applications/
 
 # Fix shadowsocks-rust missing OpenSSL dependency for cross-compilation
-sed -i '/Package\/shadowsocks-rust\/Default/,/endef/{s/DEPENDS:=\$$(RUST_ARCH_DEPENDS)/DEPENDS:=+$$(RUST_ARCH_DEPENDS) +libopenssl/}' feeds/packages/net/shadowsocks-rust/Makefile
+sed -i '/Package\/shadowsocks-rust\/Default/,/endef/{s/DEPENDS:=\$(RUST_ARCH_DEPENDS)/DEPENDS:=+\$(RUST_ARCH_DEPENDS) +libopenssl/}' feeds/packages/net/shadowsocks-rust/Makefile
 
 # ddnstox
 cp -rf clone/ddnstox/ddnstox feeds/packages/net/
@@ -86,6 +92,15 @@ cp -rf clone/ddnstox/luci-app-ddnstox feeds/luci/applications/
 # bandix
 cp -rf clone/openwrt-bandix/openwrt-bandix feeds/packages/net/
 cp -rf clone/luci-app-bandix/luci-app-bandix feeds/luci/applications/
+
+# openlist2
+cp -rf clone/openlist2/openlist2 feeds/packages/net/
+cp -rf clone/openlist2/luci-app-openlist2 feeds/luci/applications/
+
+# nikki
+cp -rf clone/nikki/nikki feeds/packages/net/
+cp -rf clone/nikki/luci-app-nikki feeds/luci/applications/
+cp -rf clone/nikki/mihomo-meta feeds/packages/net/
 
 # ---- Remove conflicting package ----
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
